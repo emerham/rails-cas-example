ee398429117873464763e02c6940fa26c5b02431
Matthew Brabham <brabham.matthew@gmail.com>
2020-08-04 08:39:32 -0700

    Removing default index.html and updating controllers and views to get the active user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  helper_method :current_user
+  helper_method :logged_in?
   def index
   end
 
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,6 @@
 class LoginController < ApplicationController
   before_filter CAS::Filter
   def index
+    redirect_to('/')
   end
-
 end
